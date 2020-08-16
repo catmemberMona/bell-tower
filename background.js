8e38abdcb11adfd340acee506b3f4ac21ba997c6
Mona Zheng <62810761+catmemberMona@users.noreply.github.com>
2020-08-16 14:05:28 -0400

    initial commit. making template for chrome extention

---
+++ background.js
@@ -0,0 +1,6 @@
+//always be running while your extension is turned on 
+//is useful for listening to different events, such as keyboard presses, or for navigating to different pages
+
+
+chrome.runtime.onInstalled.addListener(function() {    // add an action here
+});
