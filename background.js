0afb4c45a5353416a9665fe8ff9b043fac46f380
Mona Zheng <62810761+catmemberMona@users.noreply.github.com>
2020-08-16 21:06:28 -0400

    added audio files for different chime counts. created conditionals for differnt times and the corresponding audio

--- background.js
+++ background.js
@@ -1,9 +1,51 @@
 //always be running while your extension is turned on 
 //is useful for listening to different events, such as keyboard presses, or for navigating to different pages
-const ringBell = () => {
-  let audio = new Audio(chrome.runtime.getURL("./clock-bell-chimes.wav"));
+const bellChimes = () => {
+
+}
+
+
+const ringBell = (hour) => {
+  let audio = new Audio(chrome.runtime.getURL("./clock-bell-twelve.wav")); // default 
+  switch (hour % 12) {
+    case 1:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-one.wav"));
+      break;
+    case 2:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-two.wav"));
+      break;
+    case 3:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-three.wav"));
+      break;
+    case 4:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-four.wav"));
+      break;
+    case 5:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-five.wav"));
+      break;
+    case 6:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-six.wav"));
+      break;
+    case 7:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-seven.wav"));
+      break;
+    case 8:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-eight.wav"));
+      break;
+    case 9:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-nine.wav"));
+      break;
+    case 10:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-ten.wav"));
+      break;
+    case 11:
+      audio = new Audio(chrome.runtime.getURL("./clock-bell-eleven.wav"));
+      break;``
+  
+  }
   audio.play();
 }
+
 
 const firstBellTime = () => {
   const now = new Date();
@@ -18,7 +60,9 @@ chrome.runtime.onInstalled.addListener(function() {    // add an action here
 
   const firstBell = firstBellTime();
   setTimeout(function(){
-    ringBell();
+    const now = new Date();
+    const hour = now.getHours();
+    ringBell(hour);
     setInterval(ringBell, 60 * 60 * 1000); // every hour
   }, firstBell);
 
